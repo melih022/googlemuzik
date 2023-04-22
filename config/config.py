80687310155005a4f76ca01d3c39dd4135deacd1
melih022 <110200865+melih022@users.noreply.github.com>
2023-04-23 01:39:54 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -81,13 +81,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
-
 UPSTREAM_REPO = getenv(
-
     "UPSTREAM_REPO",
-
     "https://github.com/melih022/demelimusic",
-
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
